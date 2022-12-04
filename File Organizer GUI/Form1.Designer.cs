dcc7f3bdf7188de6411e2a3f8d6322927c427b1b
RandomBoiDude <gabeyiscool43@gmail.com>
2022-12-04 22:15:55 +0100

    Initial Version. To be updated.

---
+++ File Organizer GUI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace File_Organizer_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.moveFilesBtn = new System.Windows.Forms.Button();
+            this.undoBtn = new System.Windows.Forms.Button();
+            this.arrangeFilesBtn = new System.Windows.Forms.Button();
+            this.pathNameLbl = new System.Windows.Forms.Label();
+            this.currentPathTxt = new System.Windows.Forms.TextBox();
+            this.infoLogLbl = new System.Windows.Forms.Label();
+            this.logsList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // moveFilesBtn
+            // 
+            this.moveFilesBtn.Location = new System.Drawing.Point(12, 338);
+            this.moveFilesBtn.Name = "moveFilesBtn";
+            this.moveFilesBtn.Size = new System.Drawing.Size(140, 40);
+            this.moveFilesBtn.TabIndex = 0;
+            this.moveFilesBtn.Text = "Move Files";
+            this.moveFilesBtn.UseVisualStyleBackColor = true;
+            this.moveFilesBtn.Click += new System.EventHandler(this.moveFilesBtn_Click);
+            // 
+            // undoBtn
+            // 
+            this.undoBtn.Location = new System.Drawing.Point(619, 338);
+            this.undoBtn.Name = "undoBtn";
+            this.undoBtn.Size = new System.Drawing.Size(140, 40);
+            this.undoBtn.TabIndex = 1;
+            this.undoBtn.Text = "Undo";
+            this.undoBtn.UseVisualStyleBackColor = true;
+            this.undoBtn.Click += new System.EventHandler(this.undoBtn_Click);
+            // 
+            // arrangeFilesBtn
+            // 
+            this.arrangeFilesBtn.Location = new System.Drawing.Point(332, 338);
+            this.arrangeFilesBtn.Name = "arrangeFilesBtn";
+            this.arrangeFilesBtn.Size = new System.Drawing.Size(140, 40);
+            this.arrangeFilesBtn.TabIndex = 2;
+            this.arrangeFilesBtn.Text = "Arrange Files";
+            this.arrangeFilesBtn.UseVisualStyleBackColor = true;
+            this.arrangeFilesBtn.Click += new System.EventHandler(this.arrangeFilesBtn_Click);
+            // 
+            // pathNameLbl
+            // 
+            this.pathNameLbl.AutoSize = true;
+            this.pathNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pathNameLbl.Location = new System.Drawing.Point(9, 41);
+            this.pathNameLbl.Name = "pathNameLbl";
+            this.pathNameLbl.Size = new System.Drawing.Size(88, 15);
+            this.pathNameLbl.TabIndex = 3;
+            this.pathNameLbl.Text = "Current Folder:";
+            // 
+            // currentPathTxt
+            // 
+            this.currentPathTxt.Location = new System.Drawing.Point(103, 40);
+            this.currentPathTxt.Name = "currentPathTxt";
+            this.currentPathTxt.Size = new System.Drawing.Size(410, 20);
+            this.currentPathTxt.TabIndex = 4;
+            // 
+            // infoLogLbl
+            // 
+            this.infoLogLbl.AutoSize = true;
+            this.infoLogLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLogLbl.Location = new System.Drawing.Point(12, 80);
+            this.infoLogLbl.Name = "infoLogLbl";
+            this.infoLogLbl.Size = new System.Drawing.Size(54, 15);
+            this.infoLogLbl.TabIndex = 5;
+            this.infoLogLbl.Text = "Info Log:";
+            // 
+            // logsList
+            // 
+            this.logsList.FormattingEnabled = true;
+            this.logsList.Location = new System.Drawing.Point(12, 111);
+            this.logsList.Name = "logsList";
+            this.logsList.Size = new System.Drawing.Size(747, 199);
+            this.logsList.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 390);
+            this.Controls.Add(this.logsList);
+            this.Controls.Add(this.infoLogLbl);
+            this.Controls.Add(this.currentPathTxt);
+            this.Controls.Add(this.pathNameLbl);
+            this.Controls.Add(this.arrangeFilesBtn);
+            this.Controls.Add(this.undoBtn);
+            this.Controls.Add(this.moveFilesBtn);
+            this.Name = "Form1";
+            this.Text = "File Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button moveFilesBtn;
+        private System.Windows.Forms.Button undoBtn;
+        private System.Windows.Forms.Button arrangeFilesBtn;
+        private System.Windows.Forms.Label pathNameLbl;
+        private System.Windows.Forms.TextBox currentPathTxt;
+        private System.Windows.Forms.Label infoLogLbl;
+        private System.Windows.Forms.ListBox logsList;
+    }
+}
+
---
+++ File Organizer GUI/Form1.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Organizer_GUI
+{
+    public partial class Form1 : Form
+    {
+        //Gets the user path of whoever is currently logged in on the computer.
+        static string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        static List<string> files = new List<string>();
+        static string movedFiles = Path.Combine(userPath, "Documents", "storage.txt");
+        static string createdFolders = Path.Combine(userPath, "Documents", "folders.txt");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void moveFilesBtn_Click(object sender, EventArgs e)
+        {
+            //Checks if inputted path is valid
+            if (!Directory.Exists(currentPathTxt.Text) ||
+                string.IsNullOrWhiteSpace(currentPathTxt.Text))
+            {
+                logsList.Items.Add("Error: Please input a valid path.");
+                return;
+            }
+
+            //Disable all buttons until function has been completed.
+            moveFilesBtn.Enabled = false;
+            arrangeFilesBtn.Enabled = false;
+            undoBtn.Enabled = false;
+
+            string sourcePath = currentPathTxt.Text;
+
+            //Get all files from main path
+            GetPaths(sourcePath);
+
+            //For each file, move to corresponding folder 
+            foreach (string file in files)
+            {
+                if (Path.GetFileName(file) != "storage.txt" &&
+                    Path.GetFileName(file) != "folders.txt")
+                {
+                    try
+                    {
+                        MoveFile(file);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            moveFilesBtn.Enabled = true;
+            arrangeFilesBtn.Enabled = true;
+            undoBtn.Enabled = true;
+        }
+
+        private void GetPaths(string path)
+        {
+            //Ensures that the list is always empty
+            if (files.Count != 0)
+                files.Clear();
+
+            try
+            {
+                string[] tempFiles = Directory.GetFiles(path);
+                foreach (string file in tempFiles)
+                {
+                    //Ensures that no files are added twice
+                    if (!files.Contains(file))
+                        files.Add(file);
+                }
+            }
+            catch (UnauthorizedAccessException e) { logsList.Items.Add($"Error: {e.Message}"); }
+        }
+
+        private void MoveFile(string filePath)
+        {
+            //Common file extensions. Too lazy to get more.
+            string[] docs = { ".doc", ".docx", ".eml", ".log", ".msg", ".odt",
+                ".pages", ".rtf", ".tex", ".txt", ".wpd", ".numbers", ".ods",
+                ".xlr", ".xls", ".xlsx" };
+            string[] compressed = { ".7z", ".cbr", ".deb", ".gz", ".pak", ".pkg",
+                ".rar", ".rpm", ".tar.gz", ".xapk", ".zip", ".zipx" };
+            string[] fonts = { ".fnt", ".otf", ".ttf", ".woff", ".woff2" };
+            string[] audios = { ".aif", ".flac", ".m3u", ".m4a", ".mid", ".mp3",
+                ".ogg", ".wav", ".wma" };
+            string[] videos = { ".3gp", ".asf", ".avi", ".flv", ".m4v", ".mov",
+                ".mp4", ".mpg", ".srt", ".swf", ".ts", ".vob", ".wmv" };
+            string[] pics = { ".bmp", ".dcm", ".dds", ".djvu", ".gif", ".heic",
+                ".jpg", ".jpeg", ".png", ".psd", ".tga", ".tif",".ai", ".cdr", ".emf",
+                ".eps", ".ps", ".sketch", ".svg", ".vsdx",".3dm", ".3ds", ".blend",
+                ".dae", ".fbx", ".max", ".obj" };
+            string[] misc = { ".crdownload", ".ics", ".msi", ".nomedia",
+                ".part", ".pkpass", ".torrent" };
+
+            //Gets the file extension
+            string ext = Path.GetExtension(filePath);
+            string destDir = "";
+
+            //Creates destination path based on file extension
+            if (docs.Contains(ext))
+                destDir = Path.Combine(userPath, "Documents", $"{ext.Remove(0, 1).ToUpper()} files");
+
+            else if (compressed.Contains(ext))
+                destDir = Path.Combine(userPath, "Documents", "Compressed", $"{ext.Remove(0, 1).ToUpper()} files");
+
+            else if (fonts.Contains(ext))
+                destDir = Path.Combine(userPath, "Documents", "Fonts", $"{ext.Remove(0, 1).ToUpper()} files");
+
+            else if (audios.Contains(ext))
+                destDir = Path.Combine(userPath, "Music");
+
+            else if (videos.Contains(ext))
+                destDir = Path.Combine(userPath, "Videos");
+
+            else if (pics.Contains(ext))
+                destDir = Path.Combine(userPath, "Pictures", $"{ext.Remove(0, 1).ToUpper()} files");
+
+            else if (ext == ".lnk")
+                destDir = Path.Combine(userPath, "Desktop");
+
+            else if (misc.Contains(ext))
+                destDir = Path.Combine(userPath, "Documents", "Miscellaneous Files", $"{ext.Remove(0, 1).ToUpper()} files");
+
+            //Terminates the function if the extension doesn't exist in any of the arrays
+            else
+                return;
+
+            //Creates subfolder if it doesn't exist
+            if (!Directory.Exists(destDir))
+            {
+                Directory.CreateDirectory(destDir);
+                using (var writer = new StreamWriter(createdFolders, true))
+                { writer.WriteLine(destDir); }
+            }
+
+            //Deletes file in subfolder if it already exists
+            if (File.Exists(Path.Combine(destDir, Path.GetFileName(filePath))))
+                File.Delete(Path.Combine(destDir, Path.GetFileName(filePath)));
+
+            try
+            {
+                //Moves file to subfolder
+                File.Move(filePath, Path.Combine(destDir, Path.GetFileName(filePath)));
+
+                //Notes file as a moved file and keeps track of new location
+                using (StreamWriter writer = new StreamWriter(movedFiles, true))
+                { writer.WriteLine($"{filePath}, {Path.Combine(destDir, Path.GetFileName(filePath))}"); }
+
+                logsList.Items.Add($"Info: {Path.GetFileName(filePath)} moved to {destDir}");
+            }
+            catch (UnauthorizedAccessException) { logsList.Items.Add($"Error: Access to the path {filePath} was denied."); }
+            catch (IOException) { }
+        }
+
+        private void arrangeFilesBtn_Click(object sender, EventArgs e)
+        {
+            //Get source path
+            if (!Directory.Exists(currentPathTxt.Text) ||
+                string.IsNullOrWhiteSpace(currentPathTxt.Text))
+            {
+                logsList.Items.Add("Error: Please input a valid path.");
+                return;
+            }
+
+            //Disable all buttons until function has been completed.
+            moveFilesBtn.Enabled = false;
+            arrangeFilesBtn.Enabled = false;
+            undoBtn.Enabled = false;
+
+            string sourcePath = currentPathTxt.Text;
+
+            //Get all files from main path
+            GetPaths(sourcePath);
+
+            //For each file, move to corresponding folder 
+            foreach (string file in files)
+            {
+                if (Path.GetFileName(file) != "storage.txt" &&
+                    Path.GetFileName(file) != "folders.txt")
+                {
+                    try
+                    {
+                        ArrangeFile(file);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            moveFilesBtn.Enabled = true;
+            arrangeFilesBtn.Enabled = true;
+            undoBtn.Enabled = true;
+        }
+
+        private void ArrangeFile(string filePath)
+        {
+            //Common file extensions. Too lazy to get more.
+            string[] docs = { ".doc", ".docx", ".eml", ".log", ".msg", ".odt",
+                ".pages", ".rtf", ".tex", ".txt", ".wpd", ".numbers", ".ods",
+                ".xlr", ".xls", ".xlsx", ".ppt", ".pptx", ".csv", ".pdf" };
+            string[] compressed = { ".7z", ".cbr", ".deb", ".gz", ".pak", ".pkg",
+                ".rar", ".rpm", ".tar.gz", ".xapk", ".zip", ".zipx" };
+            string[] fonts = { ".fnt", ".otf", ".ttf", ".woff", ".woff2" };
+            string[] audios = { ".aif", ".flac", ".m3u", ".m4a", ".mid", ".mp3",
+                ".ogg", ".wav", ".wma" };
+            string[] videos = { ".3gp", ".asf", ".avi", ".flv", ".m4v", ".mov",
+                ".mp4", ".mpg", ".srt", ".swf", ".ts", ".vob", ".wmv" };
+            string[] pics = { ".bmp", ".dcm", ".dds", ".djvu", ".gif", ".heic",
+                ".jpg", ".jpeg", ".png", ".psd", ".tga", ".tif",".ai", ".cdr", ".emf",
+                ".eps", ".ps", ".sketch", ".svg", ".vsdx",".3dm", ".3ds", ".blend",
+                ".dae", ".fbx", ".max", ".obj" };
+            string[] misc = { ".crdownload", ".ics", ".msi", ".nomedia",
+                ".part", ".pkpass", ".torrent", ".iso" };
+            string[] exec = { ".apk", ".app", ".bat", ".bin", ".cmd", ".com",
+                ".exe", ".ipa", ".jar", ".run", ".sh" };
+            string[] web = { ".asp", ".aspx", ".cer", ".cfm", ".csr", ".css",
+                ".html", ".js", ".json", ".jsp", ".php", ".xhtml" };
+
+            //Gets the file extension and the current file directory
+            string folderName = Path.GetDirectoryName(filePath);
+            string ext = Path.GetExtension(filePath);
+            string destDir = "";
+            string rootDestDir = "";
+
+            //Creates destination path based on file extension
+            if (docs.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Documents");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (compressed.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Compressed Files");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (fonts.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Fonts");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (audios.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Audio Files");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (videos.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Videos");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (pics.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Pictures");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (misc.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Miscellaneous Files");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (exec.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Executables");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (web.Contains(ext))
+            {
+                rootDestDir = Path.Combine(folderName, "Web Files");
+                destDir = Path.Combine(rootDestDir, $"{ext.Remove(0, 1).ToUpper()} files");
+            }
+
+            else if (ext == ".lnk")
+                destDir = Path.Combine(folderName, "Links");
+
+            //Terminates the function if the extension doesn't exist in any of the arrays
+            else
+                return;
+
+            //Creates subfolders if they doesn't exist
+            if (!Directory.Exists(rootDestDir))
+            {
+                Directory.CreateDirectory(rootDestDir);
+                using (var writer = new StreamWriter(createdFolders, true))
+                { writer.WriteLine(rootDestDir); }
+            }
+
+            if (!Directory.Exists(destDir))
+            {
+                Directory.CreateDirectory(destDir);
+                using (var writer = new StreamWriter(createdFolders, true))
+                { writer.WriteLine(destDir); }
+            }
+
+            //Deletes file in subfolder if it already exists
+            if (File.Exists(Path.Combine(destDir, Path.GetFileName(filePath))))
+                File.Delete(Path.Combine(destDir, Path.GetFileName(filePath)));
+
+            try
+            {
+                //Moves file to subfolder
+                File.Move(filePath, Path.Combine(destDir, Path.GetFileName(filePath)));
+
+                //Notes file as a moved file and keeps track of new location
+                using (StreamWriter writer = new StreamWriter(movedFiles, true))
+                { writer.WriteLine($"{filePath}, {Path.Combine(destDir, Path.GetFileName(filePath))}"); }
+
+                logsList.Items.Add($"Info: {Path.GetFileName(filePath)} moved to {destDir}");
+            }
+            catch (UnauthorizedAccessException) { logsList.Items.Add($"Error: Access to the path {filePath} was denied."); }
+            catch (IOException) { }
+        }
+
+        private void undoBtn_Click(object sender, EventArgs e)
+        {
+            //Checks if file exists and isn't empty
+            if (File.Exists(movedFiles) && !IsTextFileEmpty(movedFiles))
+            {
+                //Disable all buttons until function has been completed.
+                moveFilesBtn.Enabled = false;
+                arrangeFilesBtn.Enabled = false;
+                undoBtn.Enabled = false;
+
+                //Passes each file path in the text file as a parameter to the Undo function
+                foreach (var line in File.ReadLines(movedFiles))
+                    Undo(line);
+
+                //Empties list
+                if (files.Count != 0)
+                    files.Clear();
+
+                //Clears all text in file
+                var write = new StreamWriter(movedFiles, false);
+                write.Write(string.Empty);
+                write.Close();
+
+                //Deletes any created folders
+                if (File.Exists(createdFolders) && !IsTextFileEmpty(createdFolders))
+                {
+                    foreach (var line in File.ReadLines(createdFolders))
+                        DeleteFolder(line);
+
+                    //Clears all text in file
+                    write = new StreamWriter(createdFolders, false);
+                    write.Write(string.Empty);
+                    write.Close();
+                }
+
+                logsList.Items.Add("Info: All files have been returned to their original locations.");
+
+                moveFilesBtn.Enabled = true;
+                arrangeFilesBtn.Enabled = true;
+                undoBtn.Enabled = true;
+            }
+            else
+                logsList.Items.Add("Info: No files have been moved.");
+        }
+
+        //Checks if text file is truly empty
+        private bool IsTextFileEmpty(string fileName)
+        {
+            var info = new FileInfo(fileName);
+            if (info.Length == 0)
+                return true;
+
+            // Checks if Bit Format character is the only thing in the file
+            if (info.Length < 6)
+            {
+                var content = File.ReadAllText(fileName);
+                return content.Length == 0;
+            }
+            return false;
+        }
+
+        //Moves all files back to their previous locations
+        private void Undo(string file)
+        {
+            //Creates an array whose elements are the strings before and after
+            //the comma in the text file
+            string[] locations = file.Split(',');
+
+            //Stops method if source file isn't found
+            if (!File.Exists(locations[1]))
+                return;
+
+            //Delete destination file if it already exists
+            if (File.Exists(locations[0]))
+                File.Delete(locations[0]);
+
+            //Move file to previous location
+            File.Move(locations[1], locations[0]);
+        }
+
+        private void DeleteFolder(string directory)
+        {
+            //Exits method if folder has already been deleted
+            if (!Directory.Exists(directory))
+                return;
+
+            //Deletes folder and subfolders/files
+            Directory.Delete(directory, true);
+        }
+    }
+}
